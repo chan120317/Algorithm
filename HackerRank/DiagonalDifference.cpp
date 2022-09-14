e4dd7fdbfafaa43b87d9121387190aa1324955d8
chan120317 <chan120317@outlook.kr>
2022-09-15 00:47:40 +0900

    using git bash

---
+++ HackerRank/DiagonalDifference.cpp
@@ -0,0 +1,23 @@
+﻿#include <bits/stdc++.h>
+#define FIO cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+using namespace std;
+
+/*
+* Platform : HackerRank
+* Title : Time Conversion
+* Rank : Basic
+*/
+
+string timeConversion(string s) {
+    int h = stoi(s.substr(0, 2));
+    if (h == 12) h = 0;
+    if (s.substr(8, 2) == "PM") h += 12;
+    return to_string(h) + s.substr(2, 6);
+}
+
+int main() {
+    FIO;
+    string Time;
+    cin >> Time;
+    cout << timeConversion(Time);
+}
---
+++ Programmers/makeAll0.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
+vector<int> solution(vector<string> operations) {
+    vector<int> answer;
+
+    set <string> s;
+    for (auto here : operations) {
+        char ope = here[0];
+        string num = here.substr(here.begin() + 2, here.size() - 2);
+        if (ope == 'I') s.insert(num);
+        else {
+            if (s.empty()) continue;
+            if (num == "1") s.erase(s.end() - 1);
+            else if (num == "-1") s.erase(s.begin());
+        }
+    }
+
+    if (s.empty()) answer = { 0,0 };
+    else answer = { s.begin(), s.end() - 1 };
+    return answer;
+}
+
+int main() {
+    vector <string> operations = { "I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1" };
+    solution(operations);
+}
